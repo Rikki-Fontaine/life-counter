640f54ae693e5a5a7d33af7506819fe08c48abdf
Rikki-Fontaine <146212036+Rikki-Fontaine@users.noreply.github.com>
2023-10-19 10:44:25 -0400

    stacked command damage coounters

--- src/components/commandDisplay.svelte
+++ src/components/commandDisplay.svelte
@@ -7,9 +7,13 @@
     
 div{
     display: flex;
+    flex-direction: column;
+    align-items: center;
     width: 100%;
     justify-content: space-around;
     border: solid 2px black;
 }
+
+/* make a media query to stack the commander counters in a column on small screens
 
 </style>
