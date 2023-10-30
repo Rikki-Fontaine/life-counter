268195f881b84e8b43efc7388bb26411e06e3d4b
Rikki-Fontaine <146212036+Rikki-Fontaine@users.noreply.github.com>
2023-10-30 13:43:29 -0400

    added button gradients

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -20,11 +20,16 @@
         <button class="themeBtn" on:click={() => (open = !open)}>Themes:</button>
         {#if open}
             <div class="themeMenu">  <!--Buttons with functions to change the theme variable to reassign the class name-->
-                <p>Theme:</p>
                 <button class="azoriusBtn" on:click={() => theme = "azorius"}>Azorius</button>
                 <button class="gruulBtn" on:click={() => theme = "gruul"}>Gruul</button>
                 <button class="rakdosBtn" on:click={() => theme = "rakdos"}>Rakdos</button>
-                <button class="simicButton" on:click={() => theme = "rakdos"}>Simic</button>
+                <button class="simicBtn" on:click={() => theme = "rakdos"}>Simic</button>
+                <button class="golgariBtn" on:click={() => theme = "rakdos"}>Golgari</button>
+                <button class="selesnyaBtn" on:click={() => theme = "rakdos"}>Selesnya</button>
+                <button class="dimirBtn" on:click={() => theme = "rakdos"}>Dimir</button>
+                <button class="borosBtn" on:click={() => theme = "rakdos"}>Boros</button>
+                <button class="izzetBtn" on:click={() => theme = "rakdos"}>Izzet</button>
+                <button class="orzhovBtn" on:click={() => theme = "rakdos"}>Orzhov</button>
             </div>
         {/if}
 
@@ -62,12 +67,13 @@
         top: 25%;
         display: flex;
         flex-direction: column;
+        justify-content: center;
         gap: 1rem;
         background-color: black;
         opacity: 100%;
-        height: 50%;
+        height: 70%;
         width: 50%;
-        border-radius: 10%;
+        border-radius: 5%;
         animation: fadeIn .3s;
     }
     
@@ -98,6 +104,38 @@
 
     .gruulBtn{
         background-image: linear-gradient(#799351, #AA3A3A);
+    }
+
+    .rakdosBtn{
+        background-image: linear-gradient(#121212, #F05454);
+    }
+
+    .simicBtn{
+        background-image: linear-gradient(#2192FF, #38E54D)
+    }
+
+    .golgariBtn{
+        background-image: linear-gradient(#347A2A, #121212)
+    }
+
+    .selesnyaBtn{
+        background-image: linear-gradient(#FCF9F9, #7CBD1E)
+    }
+
+    .borosBtn{
+        background-image: linear-gradient(#DD3E3E, #F2F2F2)
+    }
+
+    .izzetBtn{
+        background-image: linear-gradient(#3F52E3, #F12D2D)
+    }
+
+    .dimirBtn{
+        background-image: linear-gradient(#121212, #00A9FF )
+    }
+
+    .orzhovBtn{
+        background-image: linear-gradient(#121212, #F2F2F2);
     }
 
     h1{
